9158e37276f7fbfb0cc2906dbf7b638f0938a1d6
EphraimPillar <g20p0425@campus.ru.ac.za>
2022-05-06 14:39:43 +0200

    Sign up form done

--- signup.php
+++ signup.php
@@ -17,8 +17,42 @@
 			<a href="signin.php">Sign In</a>
         </div>			
 	</div>
-    <div class="wrapper">
-        This is the body of the sign up page.
+    <div class="main">
+        <div class="form">
+            
+            <div id="form-header">
+                <h2>Sign up</h2>
+            </div>
+
+            <form action="">
+                <div class="form-element">
+                    <label for="fname">First Name</label> 
+                    <input type="text" placeholder="Enter first name" name="fname"><br>
+                </div>    
+    
+                <div class="form-element">
+                    <label for="lname">Last Name</label>
+                    <input type="text" placeholder="Enter last name" name="lname"><br>
+                </div>
+    
+                <div class="form-element">
+                    <label>Email</label>
+                    <input type="text" placeholder="Enter email" name="fname"><br>
+                </div>
+                
+                <div class="form-element">
+                    <label>Password</label>
+                    <input type="password" placeholder="Enter password" name="fname"><br>
+                </div>
+
+                <div class="form-element">
+                    <button type="button" class="button">Sign Up</button>
+                </div>
+            </form>
+        </div>
     </div>
+    <footer>
+        <p>&#169 2022 JEW. All rights reserved.</p>
+    </footer>
 </body>
 </html>
