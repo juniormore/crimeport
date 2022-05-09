7d86502fc8221be7e168707c7537f9722ba8f695
juniormore <72398724+juniormore@users.noreply.github.com>
2022-05-09 07:01:40 +0200

    Added sql connection

--- signin.php
+++ signin.php
@@ -27,18 +27,37 @@
             <form action="">
                 <div class="form-element">
                     <label>Email</label>
-                    <input type="text" placeholder="Enter email" name="fname"><br>
+                    <input type="text" placeholder="Enter email" name="email"><br>
                 </div>
                 
                 <div class="form-element">
                     <label>Password</label>
-                    <input type="password" placeholder="Enter password" name="fname"><br>
+                    <input type="password" placeholder="Enter password" name="password"><br>
                 </div>
 
                 <div class="form-element">
-                    <button type="button" class="button">Sign In</button>
+                    <input class="button" type="submit" value="Sign in">
                 </div>
             </form>
+            <?php
+            if(isset($_POST['email']))
+            {
+                $connection=mysqli_connect('127.0.0.1', 'root', 'juniorm', 'crimeport', '4000') or die('Failed'.mysqli_error());
+		    $query="SELECT fname FROM members WHERE 
+				(email='".$_POST['email']."') AND (password='".md5($_POST['password'])."')";
+		    $result=mysqli_query($query);
+		    if(mysqli_num_rows($result)==1)
+		    {
+			    echo 'Credentials valid. Not really sure where we go from here...';
+		    }
+            else
+            {
+                echo "Invalid login details. You suck.";   
+            }
+		    mysqli_close();
+            }
+            
+        ?>
         </div>
     </div>
     <footer>
