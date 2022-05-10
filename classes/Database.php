0821bfb5cdf2d8bfaaea32cf9befe9b1bd3a45b0
juniormore <jmoremong@hotmail.com>
2022-05-10 14:34:47 +0200

    Update Database.php

--- classes/Database.php
+++ classes/Database.php
@@ -1,69 +1,16 @@
 <?php
+include 'config.inc.php';
 class Database{
-    private $host = DB_HOST;
-    private $user = DB_USER;
-    private $pass = DB_PASS;
-    private $dbname = DB_NAME;
-
+    
     private $dbh;
-    private $error;
-    private $stmt;
 
     public function __construct(){
-        //Set DSN using PDO
-        $dsn = 'mysql:host ='. $this->host .'dbname='. $this->dbname;
-
-        //Set Options (persistent connection + )
-        $options = array(
-                PDO::ATTR_PERSISTENT => true,
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        );
-
-        //PDO Instance
-        try{
-            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
-        } catch(PDOException $e) {
-            $this->error = $e->getMessage();
-        }
+        $dsh = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die('Failed'.mysql_error());
     }
 
     //method to take a query
     public function query($query){
-        $this->stmt = $this->dbh->prepare($query);
-    }
-
-    //binding values 
-    public function bind($param, $value, $type = null){
-        if(is_null($type)){
-            switch(true){
-                case is_int($value) :
-                    $type = PDO::PARAM_INT
-                    break;
-                case is_bool ($value) :
-                    $type = PDO::PARAM_BOOL;
-                    break;
-                case is_null ($value) :
-                    $type = PDO::PARAM_NULL;
-                    break;
-                default :
-                    $type = PDO::PARAM_STR;    
-            }
-        }
-        $this->stmt->bindValue($param, $value, $type);
-    }
-
-    public function execute(){
-        return $this->stmt->execute();
-    }
-
-    public function resultSet(){
-        $this->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
-    }
-
-    public function single(){
-        $this->execute();
-        return $this->stmt->fetch(PDO::FETCH_OBJ);
+        return mysql_query($query);
     }
 }
 ?>
