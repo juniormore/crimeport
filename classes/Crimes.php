01bddcf4f7a152e4e969e04b41a970905e393efb
juniormore <jmoremong@hotmail.com>
2022-05-11 04:45:51 +0200

    update crimes database access

--- classes/Crimes.php
+++ classes/Crimes.php
@@ -1,8 +1,21 @@
 <?php
 include 'config.inc.php';
-class Crimes{
-    public function getAllCrimes(){
-        
+class Crimes
+{
+    public function getAllCrimes()
+    {
+        $con=mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die('Failed'.mysqli_error());
+        $query="SELECT name FROM crimes";
+	    $result=mysql_query($query);
+	    $crimes=array();
+	    if($result)
+        {
+            while($row=mysql_fetch_array($result))
+			{
+			    $crimes[]=$row['name'];
+			}
+            return $crimes;
+        }
     }
 }
 ?>
